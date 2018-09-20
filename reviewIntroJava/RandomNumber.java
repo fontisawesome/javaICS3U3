4eac88c17c22f23bbf39b241175eb87afbf05689
fontisawesome <fontisawesome@gmail.com>
2018-09-19 21:41:52 -0400

    Q11 edit

--- reviewIntroJava/RandomNumber.java
+++ reviewIntroJava/RandomNumber.java
@@ -1,24 +1,30 @@
-// The "RandomNumber" class.
-import java.awt.*;
-import hsa.Console;
+/*
+Andy Cai and Jae Park
+Mr Rosen
+09/19/18
+Generates random number from -5 to 17 (inclusive)
+*/
 
-public class RandomNumber
+import java.awt.*; 
+import hsa.Console; //imports 
+
+public class RandomNumber //class RandomNumber
 {
-    public int randomNumber;
+    public int randomNumber;    // The random number will be stored in this variable
     static Console c;           // The output console
-    public RandomNumber(){
-	c = new Console();
+    public RandomNumber(){      // constructor
+	c = new Console();      // initialize Console variable
     }
-    public void randomNumberGeneration(){
+    public void randomNumberGeneration(){  //method that generates random number
 	
 	
-	randomNumber = (int)Math.floor(Math.random()*23 - 5);
-	c.print(randomNumber);
+	randomNumber = (int)Math.floor(Math.random()*23 - 5);  //generates random number
+	c.print(randomNumber); //prints random number
     }
-    public static void main (String[] args)
+    public static void main (String[] args) //main method
     {
-	RandomNumber d = new RandomNumber();
-	d.randomNumberGeneration();
-	// Place your program here.  'c' is the output console
-    } // main method
-} // RandomNumber class
+	RandomNumber d = new RandomNumber();  //initializing constructor
+	d.randomNumberGeneration();  //running method
+	
+    } 
+} 
