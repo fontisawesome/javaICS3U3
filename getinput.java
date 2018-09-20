7f85811ec0ef2ca7126cc15fbf79605881f5cb76
fontisawesome <fontisawesome@gmail.com>
2018-09-20 11:07:16 -0400

    Oi jae

---
+++ getinput.java
@@ -0,0 +1,12 @@
+public void getInput(){
+    c.print("Input the magnitude of the first vector: ");
+    mag1 = c.readDouble();
+    c.print("Input the angle of the first vector: ");
+    ang1 = c.readDouble();
+    c.print("Input the magnitude of the second vector: ");
+    mag2 = c.readDouble();
+    c.print("Input the angle of the second vector: ");
+    ang2 = c.readDouble();
+    c.print("Input accuracy of the answer (e.g. 0.1 to round to tenths)");
+    choice = c.readDouble();
+}
