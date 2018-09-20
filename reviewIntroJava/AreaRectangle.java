fae5644198e1617569679464b624c036b6cc8144
fontisawesome <fontisawesome@gmail.com>
2018-09-19 21:52:18 -0400

    Q12 edit

--- reviewIntroJava/AreaRectangle.java
+++ reviewIntroJava/AreaRectangle.java
@@ -1,38 +1,49 @@
-// The "AreaRectangle" class.
-import java.awt.*;
+/*
+Andy Cai and Jae Park
+Mr Rosen
+09/19/18
+Calculates area of a rectangle based on length and width
+*/
+import java.awt.*; //imports
 import hsa.Console;
 
-public class AreaRectangle
+public class AreaRectangle //class AreaRectangler
 {
     static Console c;       // The output console
-    public double length;
-    public double width;
+    public double length;   // length variable init
+    public double width;    // width variable init
     
-    public AreaRectangle(){
-	c = new Console();
+    public AreaRectangle(){ // constructor
+	c = new Console("Calculate Area of Rectangle");  // initialize Console 
     }
-    
-    public void getInput(){
+    public void title(){ //prints a title
+	String title = new String("Calculate Area of Rectangle"); //automatically centers text
+	c.print(' ', 40-title.length()/2);
+	c.println(title);
+    }
+    public void getInput(){ //gets the input
 	c.print("Insert length of rectangle here: ");
 	length = c.readDouble();
 	c.print("Insert width of rectangle here: ");
 	width = c.readDouble();
     }
     
-    public void output(){
+    public void output(){ //outputs the area, calculations done in here
 	c.println();
 	double area = length*width;
 	c.print("The area of the rectangle is " + area + ".");
     
     }
     
-    public static void main (String[] args)
+    public static void main (String[] args) //main method
     {
-	AreaRectangle d = new AreaRectangle();
+	AreaRectangle d = new AreaRectangle(); //initialize constructor
+	//calling methods
+	d.title(); 
 	d.getInput();
 	d.output();
 	
-	// Place your program here.  'c' is the output console
-    } // main method
-} // AreaRectangle class
+	
+    } 
+} 
 
